f2b820db6513823827ba0f3216df1a089ae1689d
Okiemute Gold <goldokiemute@gmail.com>
2023-02-11 12:05:59 +0100

    added sanity schemas, and created first video user, post, comment and others

--- tiktok-sanity-backend/schemas/index.ts
+++ tiktok-sanity-backend/schemas/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import user from './user'
+import post from './post'
+import comment from './comment'
+import postedBy from './postedBy'
+
+export const schemaTypes = [user, post, comment, postedBy]
