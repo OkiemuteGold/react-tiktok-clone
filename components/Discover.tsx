bfa7ef22d261a67fe7924cc36e8164b179faff11
Okiemute Gold <goldokiemute@gmail.com>
2023-02-12 02:41:04 +0100

    topics toggle active button functionality in Discover.tsx modified to use router.asPath (since I can't access router.query) instead of activeTopic state and toggleActiveButton (that doesn't update on refresh)

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+// import { useState } from "react";
 
 import { topics } from "@/utils/constants";
-import { useState } from "react";
 
 const Discover = () => {
-    const [isActive, setIsActive] = useState("coding");
+    // ----- use either the isActive state and toggleActiveButton (doesn't update on refresh) OR `router.asPath to toggle active (works since I can't access router.query)` -----
 
-    const activeTopicStyle = "xl:border-2 hover:bg-primary xl:border-[#f51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#f51997]";
+    //  ----- in div className={activeTopic === topic.name ? activeTopicStyle : topicStyle} onClick={() => toggleActiveButton(topic.name)} -----
 
-    const topicStyle = "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
+    // const [activeTopic, setActiveTopic] = useState("");
 
-    const toggleActiveButton = (topic: string) => {
-        setIsActive(topic);
-    }
+    // const toggleActiveButton = (topic: string) => {
+    //     setActiveTopic(topic);
+    // };
+
+    const router = useRouter();
+    const activePath = router.asPath;
+    // console.log(router);
+
+    const activeTopicStyle =
+        "xl:border-2 hover:bg-primary xl:border-[#f51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#f51997]";
+
+    const topicStyle =
+        "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
 
     return (
         <div className="xl:border-b-2 xl:border-gray-200 pb-6">
@@ -23,14 +33,29 @@ const Discover = () => {
             </p>
 
             <div className="flex flex-wrap gap-3">
-                {topics.map((topic) => (
-                    <Link key={topic.name} href={`/?topic/${topic.name}`}>
-                        <div className={topic.name === isActive ? activeTopicStyle + activeTopicStyle : topicStyle} onClick={() => toggleActiveButton(topic.name)}>
-                            <span className="font-bold text-2xl xl:text-md">{topic.icon}</span>
-                            <span className="font-medium text-md hidden xl:block capitalize">{topic.name}</span>
-                        </div>
-                    </Link>
-                ))}
+                {topics.map((topic) => {
+                    const { name, icon } = topic;
+                    const path = `/?topic/${name}`;
+
+                    return (
+                        <Link key={name} href={path}>
+                            <div
+                                className={
+                                    activePath === path
+                                        ? activeTopicStyle
+                                        : topicStyle
+                                }
+                            >
+                                <span className="font-bold text-2xl xl:text-md">
+                                    {icon}
+                                </span>
+                                <span className="font-medium text-md hidden xl:block capitalize">
+                                    {name}
+                                </span>
+                            </div>
+                        </Link>
+                    );
+                })}
             </div>
         </div>
     );
