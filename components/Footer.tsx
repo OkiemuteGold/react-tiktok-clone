b60098824678c17fe47ae8785bad9990d2937023
Okiemute Gold <goldokiemute@gmail.com>
2023-02-12 21:15:23 +0100

    added footer links and copyright dynamic year funcionality

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { footerList1, footerList2, footerList3 } from "@/utils/constants";
 
-const Footer = () => {
+const List = ({ items, mt }: { items: string[], mt: boolean }) => {
     return (
-        <div>
-
+        <div className={`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
+            {items.map((item) => {
+                return (
+                    <p
+                        key={item}
+                        className="text-gray-400 text-sm hover:underline cursor-pointer"
+                    >
+                        {item}
+                    </p>
+                );
+            })}
         </div>
     )
-}
+};
 
-export default Footer
+const defaultYear: any = 2023;
+const currentYear = new Date().getFullYear();
+
+const Footer = () => {
+    const [year] = useState(currentYear);
+    const [footerYear, setFooterYear] = useState(defaultYear);
+
+    useEffect(() => {
+        const timeline = `${defaultYear} - ${year}`;
+        const copyrightYear = year > defaultYear ? timeline : defaultYear;
+        setFooterYear(copyrightYear);
+    }, [year]);
+
+    return (
+        <div className="mt-6 hidden xl:block">
+            <List items={footerList1} mt={false} />
+            <List items={footerList2} mt />
+            <List items={footerList3} mt />
+
+            <p className="mt-5 text-gray-400 text-sm">Copyright@<span>{footerYear}</span> <span className="text-[#f51997]">OG</span> TikTik</p>
+        </div>
+    );
+};
+
+export default Footer;
--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SuggestedAccounts = () => {
     return (
         <div>
-
+            Suggested Accounts
         </div>
     )
 }
