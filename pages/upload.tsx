154bd75c907f422d9e53168424e29e5b48b6ddb4
Okiemute Gold <goldokiemute@gmail.com>
2023-02-20 18:49:52 +0100

    Upload page: added uploadError state, setUploadError message, modified rendering conditions in the order - 'wrongFileType > isLoading > videoAsset > uploadError.status', made GoBackComponent sub-component to return back to select file - used to close uploadError and wrongFileType messages

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -11,14 +11,32 @@ import useAuthStore from "@/store/authStore";
 import { client } from "@/utils/client";
 import { SanityAssetDocument } from "@sanity/client";
 
+export const GoBackComponent = ({ setStatus }: any) => {
+    return (
+        <button
+            type="button"
+            className="flex justify-evenly items-center w-16 mx-auto border-b-2 border-gray-300 hover:border-gray-400"
+            onClick={setStatus}
+        >
+            <FaArrowLeft className="h-3 w-3" />
+            <span>Back</span>
+        </button>
+    );
+};
+
 const Upload = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [videoAsset, setVideoAsset] = useState<SanityAssetDocument | undefined>();
     const [wrongFileType, setWrongFileType] = useState(false);
+    const [uploadError, setUploadError] = useState<any>({
+        status: false,
+        message: "",
+    });
 
     const uploadVideo = (e: any) => {
-        const selectedFile = e.target.files[0];
+        setIsLoading(true);
 
+        const selectedFile = e.target.files[0];
         const fileTypes = ["video/mp4", "video/webm", "video/ogg"];
 
         if (fileTypes.includes(selectedFile.type)) {
@@ -28,10 +46,19 @@ const Upload = () => {
                     filename: selectedFile.name,
                 })
                 .then((data) => {
-                    setVideoAsset(data);
                     setIsLoading(false);
-                }).catch((error) => {
+                    setVideoAsset(data);
+                })
+                .catch((error) => {
                     console.log(error);
+                    setIsLoading(false);
+
+                    setUploadError({
+                        status: true,
+                        message: error.isNetworkError
+                            ? "Network Error!"
+                            : error.message,
+                    });
                 });
         } else {
             setIsLoading(false);
@@ -51,83 +78,107 @@ const Upload = () => {
                     </div>
 
                     <div className="border-dashed border-4 border-gray-200 rounded-xl flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 hover:border-red-300 hover:bg-gray-100">
-                        {isLoading ? (
-                            <p className="text-center">Uploading...</p>
+                        {wrongFileType ? (
+                            <div className="text-center">
+                                <p className="text-red-500 text-lg font-semibold mb-5">
+                                    Video format not supported!
+                                </p>
+                                <p className="text-sm mb-3">
+                                    We do not currently support the format
+                                    you're trying to upload.
+                                </p>
+                                <p className="text-sm mb-5">
+                                    Please select a different video.
+                                </p>
+
+                                <GoBackComponent
+                                    setStatus={() => {
+                                        setWrongFileType(false);
+                                    }}
+                                />
+                            </div>
                         ) : (
                             <div>
-                                {videoAsset ? (
-                                    <div>
-                                        <video
-                                            src={videoAsset.url}
-                                            controls
-                                            loop
-                                            className="rounded-xl h-[450px] mt-16 bg-black"
-                                        ></video>
-                                    </div>
+                                {isLoading ? (
+                                    <p className="text-center">Uploading...</p>
                                 ) : (
                                     <div>
-                                        {!wrongFileType && (
-                                            <label htmlFor="upload-video" className="cursor-pointer">
-                                                <div className="flex flex-col items-center justify-center h-full">
-                                                    <div className="flex flex-col items-center justify-center">
-                                                        <p className="font-bold text-xl">
-                                                            <FaCloudUploadAlt className="text-gray-300 text-6xl" />
+                                        {videoAsset ? (
+                                            <div>
+                                                <video
+                                                    src={videoAsset.url}
+                                                    controls
+                                                    loop
+                                                    className="rounded-xl h-[450px] mt-16 bg-black"
+                                                ></video>
+                                            </div>
+                                        ) : (
+                                            <div>
+                                                {uploadError.status ? (
+                                                    <div>
+                                                        <p className="text-center text-red-500 text-lg font-semibold mb-5">
+                                                            {uploadError.message}
                                                         </p>
-                                                        <p className="font-bold text-xl capitalize">
-                                                            upload video
-                                                        </p>
-                                                    </div>
 
-                                                    <div className="text-gray-400 text-center">
-                                                        <p className="text-sm mt-10">
-                                                            MP4 or WebM or ogg
-                                                        </p>
-                                                        <p className="text-sm mt-6">
-                                                            720x1280 or higher
-                                                        </p>
-                                                        <p className="text-sm mt-6">
-                                                            Up to 10 minutes
-                                                        </p>
-                                                        <p className="text-sm mt-6">
-                                                            Less than 2GB
-                                                        </p>
+                                                        <GoBackComponent
+                                                            setStatus={() => {
+                                                                setUploadError({
+                                                                    status: false,
+                                                                    message: "",
+                                                                });
+                                                            }}
+                                                        />
                                                     </div>
+                                                ) : (
+                                                    <label
+                                                        htmlFor="upload-video"
+                                                        className="cursor-pointer"
+                                                    >
+                                                        <div className="flex flex-col items-center justify-center h-full">
+                                                            <div className="flex flex-col items-center justify-center">
+                                                                <p className="font-bold text-xl">
+                                                                    <FaCloudUploadAlt className="text-gray-300 text-6xl" />
+                                                                </p>
+                                                                <p className="font-bold text-xl capitalize">
+                                                                    upload video
+                                                                </p>
+                                                            </div>
 
-                                                    <div className="w-full flex mt-10">
-                                                        <span
-                                                            className="w-full bg-[#f51997] text-white border-2 border-transparent hover:bg-transparent hover:border-gray-300 hover:text-[#f51997] text-center text-base font-medium p-1.5 outline-none rounded capitalize"
-                                                        >
-                                                            Select file
-                                                        </span>
-                                                    </div>
-                                                </div>
+                                                            <div className="text-gray-400 text-center">
+                                                                <p className="text-sm mt-10">
+                                                                    MP4 or WebM or ogg
+                                                                </p>
+                                                                <p className="text-sm mt-6">
+                                                                    720x1280 or higher
+                                                                </p>
+                                                                <p className="text-sm mt-6">
+                                                                    Up to 10 minutes
+                                                                </p>
+                                                                <p className="text-sm mt-6">
+                                                                    Less than 2GB
+                                                                </p>
+                                                            </div>
 
-                                                <input
-                                                    type="file"
-                                                    name="upload-video"
-                                                    id="upload-video"
-                                                    className="w-0 h-0"
-                                                    onChange={uploadVideo}
-                                                />
-                                            </label>
+                                                            <div className="w-full flex mt-10">
+                                                                <span className="w-full bg-[#f51997] text-white border-2 border-transparent hover:bg-transparent hover:border-gray-300 hover:text-[#f51997] text-center text-base font-medium p-1.5 outline-none rounded capitalize">
+                                                                    Select file
+                                                                </span>
+                                                            </div>
+                                                        </div>
+
+                                                        <input
+                                                            type="file"
+                                                            name="upload-video"
+                                                            id="upload-video"
+                                                            className="w-0 h-0"
+                                                            onChange={uploadVideo}
+                                                        />
+                                                    </label>
+                                                )}
+                                            </div>
                                         )}
                                     </div>
                                 )}
-                            </div>
-                        )}
-
-                        {wrongFileType && (
-                            <div className="text-center">
-                                <p className="text-red-500 text-lg font-semibold mb-5">Video format not supported!</p>
-                                <p className="text-sm mb-3">We do not currently support the format you're trying to upload.</p>
-                                <p className="text-sm mb-5">Please select a different video.</p>
-
-                                <button type="button" className="flex justify-evenly items-center w-16 mx-auto border-b-2 border-gray-300 hover:border-gray-400" onClick={() => {
-                                    setWrongFileType(false)
-                                }}>
-                                    <FaArrowLeft className="h-3 w-3" />
-                                    <span>Back</span>
-                                </button>
                             </div>
                         )}
                     </div>
